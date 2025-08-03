6d007310396be3a0a97d6d3b5a49d08e0c5578a5
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2025-08-03 17:21:58 +0500

    Update route.ts

--- app/api/review/[id]/route.ts
+++ app/api/review/[id]/route.ts
@@ -3,31 +3,47 @@ import { connectDataBase } from "@/lib/db";
 import Review from "@/models/review";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     await connectDataBase();
 
     const vehicleId = params.id;
-    console.log("Vehicle ID:", vehicleId);
 
     if (!vehicleId) {
-      return NextResponse.json({ message: "Vehicle ID parameter is missing." }, { status: 400 });
+      return NextResponse.json(
+        { message: "Vehicle ID parameter is missing." },
+        { status: 400 }
+      );
     }
 
     if (!mongoose.Types.ObjectId.isValid(vehicleId)) {
-      return NextResponse.json({ message: "Invalid vehicle ID." }, { status: 400 });
+      return NextResponse.json(
+        { message: "Invalid vehicle ID." },
+        { status: 400 }
+      );
     }
 
     const reviews = await Review.find({ Vehicle_id: vehicleId });
-    // console.log(reviews)
 
     if (!reviews || reviews.length === 0) {
-      return NextResponse.json({ message: "No reviews found for this vehicle." }, { status: 404 });
+      return NextResponse.json(
+        { message: "No reviews found for this vehicle." },
+        { status: 404 }
+      );
     }
 
-    return NextResponse.json({ message: "Reviews found", data: reviews });
+    return NextResponse.json({
+      message: "Reviews retrieved successfully.",
+      data: reviews,
+    });
   } catch (error) {
-    console.error("Failed to get reviews:", error);
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    console.error("Error fetching reviews:", error);
+    return NextResponse.json(
+      { message: "Internal server error." },
+      { status: 500 }
+    );
   }
 }
